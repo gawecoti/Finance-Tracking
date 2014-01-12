e41e10cc50e3bd85ad8f0d25da482d6d4982632a
Timothy Gaweco <tim.gaweco@gmail.com>
2014-01-11 22:26:11 -0500

    added css and updated js file

--- main.js
+++ main.js
@@ -1,17 +1,30 @@
 $(document).ready(function() {
+
 	$("#target").submit(function(event) {
 		var transaction = $('#transaction').val();
 		var amount = $('#amount').val();
 
-		var now = new Date();
-		var month = now.getMonth();
-		var date = now.getDate();
-		var year = now.getFullYear();
+		if (transaction!=null && amount!=null) {
+			alert("Fill out all details");
+		}
 
-		var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+		else {
+			var now = new Date();
+			var month = now.getMonth();
+			var date = now.getDate();
+			var year = now.getFullYear();
 
-		$(".transactions table tbody").append("<tr><td>" + monthNames[month] + " " + date + ", " + year + "</td>" + "<td>" 
-		+ transaction + "</td>" + "<td>$" + amount + "</td></tr>");
+			var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+			$(".transactions table tbody").append("<tr><td>" + monthNames[month] + " " + date + ", " + year + "</td>" + "<td>" 
+			+ transaction + "</td>" + "<td>$" + amount + "</td></tr>");
+		}
+
 		event.preventDefault();
 	});
+
+
+
+
+
 });
