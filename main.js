23de090df6e05201bae2e7ebafe6afb29eae0060
Timothy Gaweco <tim.gaweco@gmail.com>
2014-01-11 22:42:16 -0500

    updated total computation in main.js

--- main.js
+++ main.js
@@ -4,27 +4,32 @@ $(document).ready(function() {
 		var transaction = $('#transaction').val();
 		var amount = $('#amount').val();
 
-		if (transaction!=null && amount!=null) {
+		// Check if there's input 
+		if (transaction=='' && amount=='') {
 			alert("Fill out all details");
 		}
 
+		// Compute transaction
 		else {
 			var now = new Date();
 			var month = now.getMonth();
 			var date = now.getDate();
 			var year = now.getFullYear();
 
-			var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+			var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
+			// Add row to transaction table
 			$(".transactions table tbody").append("<tr><td>" + monthNames[month] + " " + date + ", " + year + "</td>" + "<td>" 
 			+ transaction + "</td>" + "<td>$" + amount + "</td></tr>");
+
+			// Update total amount
+			total = $('#total').html();
+			new_total = parseFloat(total) + parseFloat(amount);
+			$('#total').html(new_total);
 		}
 
 		event.preventDefault();
 	});
-
-
-
 
 
 });
